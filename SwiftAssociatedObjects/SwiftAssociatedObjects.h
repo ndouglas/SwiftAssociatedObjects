489a997221337683cb8c77bd073cd62f8b036c47
Nathan Douglas <ndouglas@devontechnologies.com>
2015-11-12 12:10:37 -0600

    Adds project.

---
+++ SwiftAssociatedObjects/SwiftAssociatedObjects.h
@@ -0,0 +1,19 @@
+//
+//  SwiftAssociatedObjects.h
+//  SwiftAssociatedObjects
+//
+//  Created by Nathan Douglas on 11/12/15.
+//  Copyright © 2015 Nathan Douglas. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftAssociatedObjects.
+FOUNDATION_EXPORT double SwiftAssociatedObjectsVersionNumber;
+
+//! Project version string for SwiftAssociatedObjects.
+FOUNDATION_EXPORT const unsigned char SwiftAssociatedObjectsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftAssociatedObjects/PublicHeader.h>
+
+
